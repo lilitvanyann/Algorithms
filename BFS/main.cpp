91f5b1ee4ab53744590058720786f993e5608bed
Lilit Vanyan <lilitvanyann@gmail.com>
2026-05-03 23:49:30 +0400

    Add BFS implementation in main.cpp
    Implement BFS algorithm and graph representation.

---
+++ BFS/main.cpp
@@ -0,0 +1,27 @@
+#include "bfs.h"
+#include "print_bfs.h"
+
+int main()
+{
+	graph g(11);
+	g.addEdge(8, 2);
+	g.addEdge(8, 5);
+	g.addEdge(8, 7);
+	g.addEdge(7, 2);
+	g.addEdge(7, 5);
+	g.addEdge(6, 5);
+	g.addEdge(5, 2);
+	g.addEdge(9, 3);
+	g.addEdge(1, 4);
+	g.addEdge(1, 2);
+	g.addEdge(4, 3);
+	g.addEdge(2, 3);
+	g.addEdge(3, 10);
+
+	g.printGraph();
+
+	BFSdata data=BFS(g,3);
+
+	printBFSResult(data, g.getSize(), 3);
+	
+}
